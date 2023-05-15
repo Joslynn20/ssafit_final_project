9fed8457f47f88a8d21b2419884652e792873919
taehee <th1563@naver.com>
2023-05-15 17:30:13 +0900

    [+] Add first

---
+++ src/main/java/com/ssafy/ssafit/SsafitFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafitFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafitFinalProjectApplication.class, args);
+	}
+
+}
