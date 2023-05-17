627a5d4e4e377d21a869e42cd65024ca05e75a8a
Joslynn20 <joslynnkim@gmail.com>
2023-05-17 17:49:18 +0900

    [+] 보경 Add BoardComment 기능 230517

--- src/main/java/com/ssafy/ssafit/repository/BoardRepository.java
+++ src/main/java/com/ssafy/ssafit/repository/BoardRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.ssafit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.ssafy.ssafit.domain.Board;
 
-public interface BoardRepository extends JpaRepository<Board, Long>{
-	
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
+
 }
